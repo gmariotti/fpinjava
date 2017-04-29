6895206b063ad75550540467e96de10d86b8e7c0
Guido Pio Mariotti <developer.mariotti@gmail.com>
2017-04-29 18:22:30 +0200

    Chapter 4 completed (Java)

--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_06/ComposeAll.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_06/ComposeAll.java
@@ -2,12 +2,16 @@ package com.fpinjava.recursion.exercise04_06;
 
 import java.util.List;
 
+import com.fpinjava.common.CollectionUtilities;
 import com.fpinjava.common.Function;
+
+import static com.fpinjava.common.CollectionUtilities.foldRight;
+import static com.fpinjava.common.Function.identity;
 
 
 public class ComposeAll {
 
-  static <T> Function<T, T> composeAll(List<Function<T, T>> list) {
-    throw new RuntimeException("To be implemented.");
-  }
+	static <T> Function<T, T> composeAll(List<Function<T, T>> list) {
+		return foldRight(list, identity(), x -> x::compose);
+	}
 }
--- fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_09/Memoization.java
+++ fpinjava-parent/fpinjava-recursion-exercises/src/main/java/com/fpinjava/recursion/exercise04_09/Memoization.java
@@ -1,15 +1,42 @@
 package com.fpinjava.recursion.exercise04_09;
 
+import com.fpinjava.common.CollectionUtilities;
+import com.fpinjava.common.TailCall;
+
+import java.math.BigInteger;
 import java.util.List;
+
+import static com.fpinjava.common.CollectionUtilities.append;
+import static com.fpinjava.common.CollectionUtilities.foldLeft;
+import static com.fpinjava.common.CollectionUtilities.head;
+import static com.fpinjava.common.CollectionUtilities.list;
+import static com.fpinjava.common.CollectionUtilities.tail;
+import static com.fpinjava.common.TailCall.ret;
+import static com.fpinjava.common.TailCall.sus;
 
 
 public class Memoization {
 
-  public static String fibo(int number) {
-    throw new RuntimeException("To be implemented.");
-  }
+	public static String fibo(int number) {
+		List<BigInteger> list = fibo_(list(BigInteger.ZERO),
+				BigInteger.ONE, BigInteger.ZERO, BigInteger.valueOf(number)).eval();
+		return makeString(list, ", ");
+	}
 
-  public static <T> String makeString(List<T> list, String separator) {
-    throw new RuntimeException("To be implemented.");
-  }
+	private static TailCall<List<BigInteger>> fibo_(List<BigInteger> acc, BigInteger acc1,
+	                                                BigInteger acc2, BigInteger x) {
+		return x.equals(BigInteger.ZERO)
+				? ret(acc)
+				: x.equals(BigInteger.ONE)
+				? ret(append(acc, acc1.add(acc2)))
+				: sus(() -> fibo_(append(acc, acc1.add(acc2)), acc2, acc1.add(acc2), x.subtract(BigInteger.ONE)));
+	}
+
+	public static <T> String makeString(List<T> list, String separator) {
+		return list.isEmpty()
+				? ""
+				: tail(list).isEmpty()
+				? head(list).toString()
+				: head(list) + foldLeft(tail(list), "", x -> y -> x + separator + y);
+	}
 }
