92d3e29d1d8d7e43e1e7ba3ff4ca37cd31d5a8d9
Guido Pio Mariotti <developer.mariotti@gmail.com>
2017-06-13 18:53:12 +0200

    Chapter 6 completed (Java)

--- fpinjava-parent/fpinjava-optionaldata-exercises/src/main/java/com/fpinjava/optionaldata/exercise06_07/Variance.java
+++ fpinjava-parent/fpinjava-optionaldata-exercises/src/main/java/com/fpinjava/optionaldata/exercise06_07/Variance.java
@@ -3,17 +3,20 @@ package com.fpinjava.optionaldata.exercise06_07;
 
 import com.fpinjava.common.Function;
 import com.fpinjava.common.List;
-import com.fpinjava.common.TailCall;
 import com.fpinjava.optionaldata.exercise06_06.Option;
-
-import static com.fpinjava.common.TailCall.*;
 
 public class Variance {
 
-  static Function<List<Double>, Double> sum = null; // Implement this function
+	static Function<List<Double>, Double> sum =
+			ds -> ds.foldLeft(0.0, x -> y -> x + y);
 
-  static Function<List<Double>, Option<Double>> mean = null; // Implement this function
+	static Function<List<Double>, Option<Double>> mean =
+			ds -> ds.isEmpty()
+					? Option.none()
+					: Option.some(sum.apply(ds) / ds.length());
 
-  static Function<List<Double>, Option<Double>> variance = null; // Implement this function
+	static Function<List<Double>, Option<Double>> variance =
+			ds -> mean.apply(ds)
+			          .flatMap(m -> mean.apply(ds.map(x -> Math.pow(x - m, 2))));
 }
 
